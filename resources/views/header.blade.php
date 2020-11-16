a0ca521fece242ff705f03d5d25e640f9d82e306
saroj-tamang <59418116+saroj-tamang@users.noreply.github.com>
2020-11-16 23:32:44 +0545

    login page done

---
+++ resources/views/header.blade.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">Demozon</a>
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Orders</a>
+      </li>
+    </ul>
+    <form class="form-inline" action="/action_page.php">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-success" type="submit">Search</button>
+    </form>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Add Cart
+            </a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+
+
+
+{{-- <nav class="navbar navbar-default bg-success">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Ecommerce</a>
+        </div>
+    <div class="navbar" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active">
+          <a  href="#">Home</a>
+        </li>
+        <li class="active">
+          <a  href="#">Order</a>
+        </li>   
+      </ul>
+      <form class="navbar-form navbar-left">
+          <div class="form-group">
+            <input class="form-control" type="text" placeholder="Search">
+          </div>
+        <button class="btn btn-default" type="submit">Search</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="">Add Cart</a>
+          </li>
+      </ul>   
+    </div>
+   </div>
+  </nav> --}}
