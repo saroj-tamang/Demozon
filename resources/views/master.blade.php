2959f7a4af84ea6a3edb43fbbe383457edc3fc53
saroj-tamang <59418116+saroj-tamang@users.noreply.github.com>
2020-11-17 11:48:43 +0545

    login second done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Master page header</title>
+        <title>Demozon</title>
     </head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -15,11 +15,11 @@
 
 <script>
 
-    $(document).ready(function(){
-        $("button").click(function(){
-            alert("all set");
-        })
-    })
+    // $(document).ready(function(){
+    //     $("button").click(function(){
+    //         alert("all set");
+    //     })
+    // })
 </script>
 <style>
     .custom-login{
